77baa38b541abcfd44af63d8f528ee89eddbc131
Eduard Teixeira <eduardteixeira01@gmail.com>
2024-07-12 16:55:52 -0300

    melhoria nos menus

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function copyText() {
+
+  var pix =
+    "00020126750014br.gov.bcb.pix0111415622178440238Giovana  Eduard  Presente de Casamento5204000053039865802BR5915EDUARD TEIXEIRA6006FRANCA62550517PresenteCasamento50300017br.gov.bcb.brcode01051.0.063040516";
+
+  navigator.clipboard.writeText(pix);
+
+  alert("Pix Copia e Cola copiado com sucesso.");
+}
