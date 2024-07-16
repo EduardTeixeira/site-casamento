afa0e38414182936036bfae55de561487facf899
Eduard Teixeira <eduardteixeira01@gmail.com>
2024-07-16 16:40:57 -0300

    layout final

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 function copyText() {
-
   var pix =
     "00020126750014br.gov.bcb.pix0111415622178440238Giovana  Eduard  Presente de Casamento5204000053039865802BR5915EDUARD TEIXEIRA6006FRANCA62550517PresenteCasamento50300017br.gov.bcb.brcode01051.0.063040516";
 
